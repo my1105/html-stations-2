bbd9843c28c676adfdea969e71776f88a35572c7
mayu <ykmshiss@gmail.com>
2025-06-30 23:51:14 +0900

    if 文

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,14 @@
-function changeBackgroundColor(e) {
-  // ここに背景色を変える処理を書く。
+function changeBackgroundColor() {
+  const toggle = document.getElementById('check');
+  const text = document.getElementById('text');
 
+  toggle.addEventListener('change', function () {
+    if (toggle.checked) {
+      text.style.backgroundColor = 'red';
+    } else {
+      text.style.backgroundColor = 'white';
+    }
+  });
 }
+
+document.addEventListener('DOMContentLoaded', changeBackgroundColor);
